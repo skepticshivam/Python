136d9abaf5e17472690d18dde77e61c4eb447157
skepticshivam <shivam.sharma9530@googlemail.com>
2016-12-07 16:31:23 +0530

    using format specifiers

---
+++ exercise-8/exec8.py
@@ -0,0 +1,5 @@
+#using format specifiers
+formatter = "%r %r %r %r"
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (formatter, formatter, formatter, formatter)
