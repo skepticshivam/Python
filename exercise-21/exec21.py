dc137de518b399b928ef252b0d42ccb34bfa6e95
skepticshivam <shivam.sharma9530@googlemail.com>
2016-12-08 14:19:30 +0530

    functions with return value

---
+++ exercise-21/exec21.py
@@ -0,0 +1,16 @@
+def add(a,b):
+	return (a+b)
+def sub(a,b):
+	return(a-b)
+def div(a,b):
+	return (a/b)
+def mul(a,b):
+	return(a*b)
+
+a=30
+b=15
+a1=add(a,b)
+a2=sub(a,b)
+a3= mul(a,b)
+a4=div(a,b)
+print a1,a2,a3,a4
