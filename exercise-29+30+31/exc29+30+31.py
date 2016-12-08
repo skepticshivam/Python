56a122d6d9e0c16b94b2e70a48936b627c7b6239
skepticshivam <shivam.sharma9530@googlemail.com>
2016-12-08 16:41:10 +0530

    if else statement in python

---
+++ exercise-29+30+31/exc29+30+31.py
@@ -0,0 +1,15 @@
+a=raw_input("a")
+b=raw_input("b")
+c=raw_input("c")
+
+if a>b:
+	if a>c:
+		print a," is largest"
+	else:
+		print c," is largest"
+else:
+	if b>c:
+		print b," is largest"
+	else:
+		print c," is largest"	
+
